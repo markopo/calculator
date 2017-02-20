3c9bc0b004b944ccde3df567c0cebbf56ecacb07
Marko Poikkimäki <markopoikkimaki@Markos-MBP.lan>
2017-02-20 20:10:00 +0100

    Calculator init

---
+++ Calculator/CalculatorModel.swift
@@ -0,0 +1,34 @@
+//
+//  CalculatorModel.swift
+//  Calculator
+//
+//  Created by Marko Poikkimäki on 2017-02-19.
+//  Copyright © 2017 Marko Poikkimäki. All rights reserved.
+//
+
+import Foundation
+
+
+class CalculatorModel {
+    
+
+    func GetNumber(_ buttonTitle:String) -> Int {
+       return Int(buttonTitle)!
+    }
+    
+    func Addition(sum:Int, number:Int) -> Int {
+        return sum + number
+    }
+    
+    func Subtraction(sum:Int,number:Int) -> Int  {
+        return sum == 0 ? number - sum : sum - number
+    }
+    
+    func Division(sum:Int, number:Int) -> Int {
+        return number > 0 && sum > 0 ? (sum / number) : number
+    }
+    
+    func MultiPlication(sum:Int, number:Int) -> Int {
+        return sum == 0 ? number : sum * number
+    }
+}
--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -10,16 +10,84 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    private var tempNum = ""
+    private var Sum:Int = 0
+
+    private let model = CalculatorModel()
+    
+    private var lastOperation = ""
+    
+    @IBOutlet weak var SumLabel: UILabel!
+    
+ 
+
+    @IBAction func buttonClick(_ sender: UIButton) {
+        let title = sender.currentTitle!
+        
+        
+        if title != "+" && title != "C" && title != "=" && title != "-" && title != "/" && title != "*" {
+            tempNum += title
+            SumLabel.text = String(tempNum)
+        }
+        else if title == "C" {
+            Sum = 0
+            tempNum = ""
+            SumLabel.text = String(Sum)
+        }
+        else if title == "="  {
+            
+            let number = model.GetNumber(tempNum)
+            
+            
+            if lastOperation == "+" {
+                Sum = model.Addition(sum: Sum, number: number)
+            }
+            else if lastOperation == "-" {
+                Sum = model.Subtraction(sum: Sum, number: number)
+            }
+            else if lastOperation == "/" {
+                Sum = model.Division(sum: Sum, number: number)
+            }
+            else if lastOperation == "*" {
+                Sum = model.MultiPlication(sum: Sum, number: number)
+            }
+            
+            SumLabel.text = String(Sum)
+            lastOperation = ""
+            tempNum = String(Sum)
+            Sum = 0
+        }
+        else if title == "+" {
+           let number = model.GetNumber(tempNum)
+           Sum = model.Addition(sum: Sum, number: number)
+           tempNum = ""
+           SumLabel.text = String(Sum)
+           lastOperation = title
+        }
+        else if title == "-" {
+            let number = model.GetNumber(tempNum)
+            Sum = model.Subtraction(sum: Sum, number: number)
+            tempNum = ""
+            SumLabel.text = String(Sum)
+            lastOperation = title
+        }
+        else if title == "/" {
+            let number = model.GetNumber(tempNum)
+            Sum = model.Division(sum: Sum, number: number)
+            tempNum = ""
+            SumLabel.text = String(Sum)
+            lastOperation = title
+        }
+        else if title == "*" {
+            let number = model.GetNumber(tempNum)
+            Sum = model.MultiPlication(sum: Sum, number: number)
+            tempNum = ""
+            SumLabel.text = String(Sum)
+            lastOperation = title
+        }
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
+  
+    
 }
 
